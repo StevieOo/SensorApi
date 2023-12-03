9f34c1c014afd13060b40436d9d8316e312e3614
Stephen Oosting <soosting@umich.edu>
2023-12-03 10:16:07 -0600

    Fixed compiler warnings.

--- SensorApi/src/Aht10Driver.h
+++ SensorApi/src/Aht10Driver.h
@@ -6,9 +6,9 @@
 class Aht10Driver {
 private:
 	static constexpr char DEVICE_ADDRESS				= 0x38;		// Device I2C address.
-	static constexpr char INITIALIZE_COMMAND			= 0xE1;		// Intialize sensor command.
-	static constexpr char TRIGGER_MEASUREMENT_COMMAND	= 0xAC;		// Trigger sensor measurement command.
-	static constexpr char SOFT_RESET_COMMAND			= 0xBA;		// Reset sensor command.
+	static constexpr uint8_t INITIALIZE_COMMAND			= 0xE1;		// Intialize sensor command.
+	static constexpr uint8_t TRIGGER_MEASUREMENT_COMMAND	= 0xAC;		// Trigger sensor measurement command.
+	static constexpr uint8_t SOFT_RESET_COMMAND			= 0xBA;		// Reset sensor command.
 	static constexpr int RESPONSE_WAIT					= 500'000;	// Time to wait after command is sent (micro s).
 	static constexpr int BUFFER_SIZE					= 6;		// This is the size (in bytes) of the I2C buffer.  
 
